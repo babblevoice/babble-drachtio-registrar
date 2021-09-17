16da56b7a8ec532e01b4e9f6a1097b01e205ea77
barcek <barcek@gmx.com>
2021-09-17 17:52:03 +0100

    refactor: bypassed & removed lib/index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /*
   Usage:
+
   let r = new Registrar( { "srf": srf, "config": config, "userlookup": userlookup } )
 
   Get an array of domains with users registered to us, i.e. [ "bling.babblevoice.com" ]
@@ -10,8 +11,18 @@
 
   Get registrations for a user at a realm
   r.user( realm, username )
+
+  Classes
+
+  Registrar - our main class (./lib/registrar.js)
+  domain - stores a map of users who are registered (./lib/domain.js)
+  user - held by domain; stores and handles a list of registrations (./lib/user.js)
+  reg - held by user; a registration, with poss. multiple contact fields (./lib/reg.js)
+
+  Our Registrar is our external interface. Srf handles calling us to register. We then
+  provide an interface to query registrations.
 */
 
-const Registrar = require( "./lib/index.js" )
+const Registrar = require( "./lib/registrar.js" )
 
 module.exports = Registrar
