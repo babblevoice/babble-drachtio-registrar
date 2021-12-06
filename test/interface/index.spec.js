3e5ad55dc695f9f2e600dbd7e2b46b3418e00476
nick <nick@babblevoice.com>
2021-12-06 15:24:01 +0000

    auth

--- test/interface/index.spec.js
+++ test/interface/index.spec.js
@@ -1,55 +1,16 @@
 
-const crypto = require( "crypto" )
+const calculateauth = require( "../util/auth.js" ).calculateauth
 
 const should = require( "chai" ).should()
 const expect = require( "chai" ).expect
 
 const events = require( "events" )
 
-const sipauth = require( "babble-drachtio-auth" )
 const store = require( "../../lib/store.js" )
 const Request = require( "../mock/request.js" )
 
 const Registrar = require( "../../index.js" )
 
-
-function calculateauth( req, proxyauth, nc = 1 ) {
-
-  /* add a comma to simplify the regex */
-  proxyauth += ","
-  let auth = {}
-
-  auth.realm = /[,\s]{1}realm="?(.+?)[",\s]/.exec( proxyauth )[ 1 ]
-  auth.algorithm = /[,\s]{1}algorithm="?(.+?)[",\s]/.exec( proxyauth )[ 1 ]
-  auth.qop = /[,\s]{1}qop="?(.+?)[",\s]/.exec( proxyauth )[ 1 ]
-  auth.nonce = /[,\s]{1}nonce="?(.+?)[",\s]/.exec( proxyauth )[ 1 ]
-  auth.opaque = /[,\s]{1}opaque="?(.+?)[",\s]/.exec( proxyauth )[ 1 ]
-  auth.stale = "true" === /[,\s]{1}stale="?(.+?)[",\s]/.exec( proxyauth )[ 1 ]
-  auth.uri = "sip:dummy.com;transport=UDP"
-
-  /* not what the auth object was designed for, but useful */
-  let ourauth = new sipauth.auth()
-  ourauth._nonce = auth.nonce
-  ourauth._qop = auth.qop
-  ourauth._nc = nc
-  let cnonce = crypto.randomBytes( 8 ).toString( "hex" )
-  let hash = ourauth.calcauthhash( "bob", "biloxi", "dummy.com", "sip:dummy.com;transport=UDP", "REGISTER", cnonce )
-
-  let ncstr = ( "" + nc ).padStart( 8, "0" )
-
-  let authstr = `Digest username="bob",
-realm="${auth.realm}",
-nonce="${auth.nonce}",
-uri="${auth.uri}",
-qop=auth,
-algorithm=MD5,
-nc=${ncstr},
-cnonce="${cnonce}",
-response="${hash}",
-opaque="${auth.opaque}"`
-
-  req.set( "Proxy-Authorization", authstr )
-}
 
 describe( "interface", function() {
   it( `register, auth, event then check store`, async function() {
---
+++ test/interface/store.spec.js
@@ -0,0 +1,96 @@
+
+const calculateauth = require( "../util/auth.js" ).calculateauth
+
+const should = require( "chai" ).should()
+const expect = require( "chai" ).expect
+
+
+const store = require( "../../lib/store.js" )
+const Request = require( "../mock/request.js" )
+
+const registrar = require( "../../index.js" )
+const events = require( "events" )
+
+
+describe( "store", function() {
+  it( `register then check more details on our store for regstration`, async function() {
+
+    /*
+    |---------reg (expires 3600)------>|(1)
+    |<--------407 proxy auth-----------|(2)
+    |-----reg ( expires 3600 w-auth)-->|(3)
+    |<--------200 ok-------------------|(4)
+    Then check our store public interface
+    */
+    let ourevents = []
+    let cb
+    let options = {
+        srf: {
+          use: ( method, fn ) => {
+            expect( method ).to.equal( "register" )
+            cb = fn
+          }
+      },
+      userlookup: ( username, realm ) => {
+        expect( username ).to.equal( "bob" )
+        expect( realm ).to.equal( "dummy.com" )
+        return {
+          "secret": "biloxi",
+          "display": "Kermit Frog"
+        }
+      },
+      em: new events.EventEmitter()
+    }
+    const r = new registrar( options )
+
+    let regevent = new Promise( ( resolve ) => {
+      options.em.on( "register", ( i ) => {
+        resolve( i )
+      } )
+    } )
+
+    let res = {
+      send: ( code, body ) => {
+        ourevents.push( { code, body } )
+      }
+    }
+
+    /* Step 1. send register */
+    let req = Request.create()
+    cb( req, res )
+
+    /* Step 3. now auth against auth request */
+    let pa = ourevents[ 0 ].body.headers[ "Proxy-Authenticate" ]
+    req = Request.create()
+    calculateauth( req, pa )
+    req.set( "Expires", "3600" )
+    cb( req, res )
+
+    let reginfo = await regevent
+
+    let userreginfo = await r.user( "dummy.com", "bob" )
+    let contacts = await r.contacts( "bob@dummy.com" )
+    /* the fuller extend of this structure is checked elsewhere */
+    expect( userreginfo ).to.be.a( "array" ).to.have.lengthOf( 1 )
+    expect( userreginfo[ 0 ].contacts ).to.be.a( "array" ).to.have.lengthOf( 1 )
+
+    expect( contacts ).to.be.a( "object" )
+    expect( contacts.username ).to.be.a( "string" ).to.equal( "bob" )
+    expect( contacts.realm ).to.be.a( "string" ).to.equal( "dummy.com" )
+    expect( contacts.display ).to.be.a( "string" ).to.equal( "Kermit Frog" )
+    expect( contacts.uri ).to.be.a( "string" ).to.equal( "bob@dummy.com" )
+    expect( contacts.contacts ).to.be.a( "array" ).to.have.lengthOf( 1 )
+    expect( contacts.contacts[ 0 ] ).to.be.a( "string" ).to.equal( "sip:1000@192.168.0.141:59095;rinstance=302da93c3a2ae72b;transport=UDP" )
+
+    userreginfo = await r.user( { "realm": "dummy.com", "username": "bob" } )
+    expect( userreginfo ).to.be.a( "array" ).to.have.lengthOf( 1 )
+    expect( userreginfo[ 0 ].contacts ).to.be.a( "array" ).to.have.lengthOf( 1 )
+
+    userreginfo = await r.user( { "uri": "bob@dummy.com" } )
+    expect( userreginfo ).to.be.a( "array" ).to.have.lengthOf( 1 )
+    expect( userreginfo[ 0 ].contacts ).to.be.a( "array" ).to.have.lengthOf( 1 )
+
+    store.get( req ).destroy()
+
+  } )
+} )
